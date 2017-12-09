c73883cb9853e348af1976a0514704f3f58edb75
arabic-hebrew-meetings <arabic.hebrew.meetings@gmail.com>
2017-12-09 17:42:47 +0200

    Added more

---
+++ myScript.js
@@ -0,0 +1,17 @@
+﻿function getItem(){
+	var i = Math.floor(Math.random() * hebrew.length)
+    document.getElementById("hebrewText").innerHTML = hebrew[i];
+	document.getElementById("arabicText").innerHTML = arabic[i];
+}
+
+var hebrew = [
+"?מה הספר האחרון שקראת",
+"?אם היית יכול/ה לעבוד בכל מקצוע, באיזה מקצוע זה היה",
+"?אם היית מקבל/ת מחר 3 משאלות - מה היית בוחר"
+];
+
+var arabic = [
+"شو اخر كتاب قرأته؟",
+"اذا كنت بتقدر تشتغل بكل موضوع بأي موضوع كنت بتشتغل؟",
+"اذا بحكولك انو بحققولك تلات امنيات، شو كنت بتختار؟"
+];
