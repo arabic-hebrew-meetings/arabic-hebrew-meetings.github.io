5d2cc6b39a774d1beee998e14ac4f43277696b18
arabic-hebrew-meetings <34382029+arabic-hebrew-meetings@users.noreply.github.com>
2019-12-14 04:17:28 +0200

    Add files via upload

--- headerFooterScript.js
+++ headerFooterScript.js
@@ -1,12 +1,24 @@
 function getHeader(){
 	console.log("getHeader Start");
-	document.getElementById("header").innerHTML = `<div class="row">
-         <nav class="col-sm-10 text-left">
-    <ul class="nav navbar-nav">
-      <li class="dropdown">
-        <a class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="caret"></span>פעילויות &nbsp فعاليات</a>
-        <ul class="dropdown-menu">
-		  <li><a href="questions.html">שואלים אחד את השני  -   منسأل بعض</a></li>
+	document.getElementById("header").innerHTML = `<div class="container-fluid">
+  <ul class="nav navbar-nav navbar-left">
+  <li><a href="phrases.html">שיחון &nbsp عبارات شائعة</a></li>
+  </ul>
+    <div class="navbar-header pull-right">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand active" href="#">מפגשי ערבית עברית</a>
+    </div>
+    <div class="collapse navbar-collapse pull-right">
+      <ul class="nav navbar-nav pull-right">
+        <li class="dropdown rtl">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">פעילויות &nbsp فعاليات <b class="caret"></b></a>
+          <ul class="dropdown-menu dropdown-menu-right" style="text-align: right">
+            <li><a href="questions.html">שואלים אחד את השני  -   منسأل بعض</a></li>
 		  <li><a href="discussions.html">מועדון דיבייט  -   نادي النقاش</a></li>
 		  <li><a href="songs.html">שירים - اغاني</a></li>
           <li><a href="21questions.html">מפורסמים  -  مشاهير</a></li>
@@ -17,14 +29,12 @@ function getHeader(){
           <li><a href="slang.html">סלנג - مصطلحات عامية</a></li>
           <li><a href="sayings.html">פתגמים  -  امثال</a></li>
           <li><a href="picture.html">היכרות בעזרת תמונות - تعارف عن طريق الصور</a></li>
-        </ul>
-      </li>
-      <li><a href="phrases.html">שיחון &nbsp عبارات شائعة</a></li>
-      <li><a href="index.html">בית &nbsp بيت</a></li>
-    </ul>
-      </nav>    
-		<img class="col-sm-2" src="logo-speaklang2-03.png"/>
-    </div>`;
+          </ul>
+        </li>
+		<li class="active"><a href="index.html">בית &nbsp بيت</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div>`;
 	console.log("getHeader End");
 }
 
