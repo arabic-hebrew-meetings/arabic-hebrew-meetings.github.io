973ee6fec3b0335bece756bc8f36ec8d0c2216a2
arabic-hebrew-meetings <34382029+arabic-hebrew-meetings@users.noreply.github.com>
2020-04-24 20:47:26 +0300

    Add files via upload

--- meetingsScript.js
+++ meetingsScript.js
@@ -156,7 +156,7 @@ function displayLevelOptions(userLang) {
 		  <div class="cntr rtl">
 			<label for="rdo-1" class="btn-radio order-three level-label">
 			  <input type="radio" onclick="doSomething('Hebrew','Advanced')" id="rdo-1" name="radio-grp">
-			  <svg width="20px" height="20px" viewBox="0 0 20 20">
+			  <svg class="option-1-hebrew" width="20px" height="20px" viewBox="0 0 20 20">
 				<circle cx="10" cy="10" r="9"></circle>
 				<path d="M10,7 C8.34314575,7 7,8.34314575 7,10 C7,11.6568542 8.34314575,13 10,13 C11.6568542,13 13,11.6568542 13,10 C13,8.34314575 11.6568542,7 10,7 Z" class="inner"></path>
 				<path d="M10,1 L10,1 L10,1 C14.9705627,1 19,5.02943725 19,10 L19,10 L19,10 C19,14.9705627 14.9705627,19 10,19 L10,19 L10,19 C5.02943725,19 1,14.9705627 1,10 L1,10 L1,10 C1,5.02943725 5.02943725,1 10,1 L10,1 Z" class="outer"></path>
@@ -165,7 +165,7 @@ function displayLevelOptions(userLang) {
 			</label>
 			<label for="rdo-2" class="btn-radio order-two level-label">
 			  <input type="radio" onclick="doSomething('Hebrew','Intermediate')" id="rdo-2" name="radio-grp">
-			  <svg width="20px" height="20px" viewBox="0 0 20 20">
+			  <svg class="option-2-hebrew" width="20px" height="20px" viewBox="0 0 20 20">
 				<circle cx="10" cy="10" r="9"></circle>
 				<path d="M10,7 C8.34314575,7 7,8.34314575 7,10 C7,11.6568542 8.34314575,13 10,13 C11.6568542,13 13,11.6568542 13,10 C13,8.34314575 11.6568542,7 10,7 Z" class="inner"></path>
 				<path d="M10,1 L10,1 L10,1 C14.9705627,1 19,5.02943725 19,10 L19,10 L19,10 C19,14.9705627 14.9705627,19 10,19 L10,19 L10,19 C5.02943725,19 1,14.9705627 1,10 L1,10 L1,10 C1,5.02943725 5.02943725,1 10,1 L10,1 Z" class="outer"></path>
@@ -174,7 +174,7 @@ function displayLevelOptions(userLang) {
 			</label>
 			<label for="rdo-3" class="btn-radio order-one level-label">
 			  <input type="radio" onclick="doSomething('Hebrew','Beginner')" id="rdo-3" name="radio-grp">
-			  <svg width="20px" height="20px" viewBox="0 0 20 20">
+			  <svg class="option-3-hebrew" width="20px" height="20px" viewBox="0 0 20 20">
 				<circle cx="10" cy="10" r="9"></circle>
 				<path d="M10,7 C8.34314575,7 7,8.34314575 7,10 C7,11.6568542 8.34314575,13 10,13 C11.6568542,13 13,11.6568542 13,10 C13,8.34314575 11.6568542,7 10,7 Z" class="inner"></path>
 				<path d="M10,1 L10,1 L10,1 C14.9705627,1 19,5.02943725 19,10 L19,10 L19,10 C19,14.9705627 14.9705627,19 10,19 L10,19 L10,19 C5.02943725,19 1,14.9705627 1,10 L1,10 L1,10 C1,5.02943725 5.02943725,1 10,1 L10,1 Z" class="outer"></path>
@@ -198,7 +198,7 @@ function displayLevelOptions(userLang) {
 		  <div class="cntr rtl">
 			<label for="rdo-1" class="btn-radio order-three level-label">
 			  <input type="radio" onclick="doSomething('Arabic','Advanced')" id="rdo-1" name="radio-grp">
-			  <svg width="20px" height="20px" viewBox="0 0 20 20">
+			  <svg class="option-1-arabic" width="20px" height="20px" viewBox="0 0 20 20">
 				<circle cx="10" cy="10" r="9"></circle>
 				<path d="M10,7 C8.34314575,7 7,8.34314575 7,10 C7,11.6568542 8.34314575,13 10,13 C11.6568542,13 13,11.6568542 13,10 C13,8.34314575 11.6568542,7 10,7 Z" class="inner"></path>
 				<path d="M10,1 L10,1 L10,1 C14.9705627,1 19,5.02943725 19,10 L19,10 L19,10 C19,14.9705627 14.9705627,19 10,19 L10,19 L10,19 C5.02943725,19 1,14.9705627 1,10 L1,10 L1,10 C1,5.02943725 5.02943725,1 10,1 L10,1 Z" class="outer"></path>
@@ -207,7 +207,7 @@ function displayLevelOptions(userLang) {
 			</label>
 			<label for="rdo-2" class="btn-radio order-two level-label">
 			  <input type="radio" onclick="doSomething('Arabic','Intermediate')" id="rdo-2" name="radio-grp">
-			  <svg width="20px" height="20px" viewBox="0 0 20 20">
+			  <svg class="option-2-arabic" width="20px" height="20px" viewBox="0 0 20 20">
 				<circle cx="10" cy="10" r="9"></circle>
 				<path d="M10,7 C8.34314575,7 7,8.34314575 7,10 C7,11.6568542 8.34314575,13 10,13 C11.6568542,13 13,11.6568542 13,10 C13,8.34314575 11.6568542,7 10,7 Z" class="inner"></path>
 				<path d="M10,1 L10,1 L10,1 C14.9705627,1 19,5.02943725 19,10 L19,10 L19,10 C19,14.9705627 14.9705627,19 10,19 L10,19 L10,19 C5.02943725,19 1,14.9705627 1,10 L1,10 L1,10 C1,5.02943725 5.02943725,1 10,1 L10,1 Z" class="outer"></path>
@@ -216,7 +216,7 @@ function displayLevelOptions(userLang) {
 			</label>
 			<label for="rdo-3" class="btn-radio order-one level-label">
 			  <input type="radio" onclick="doSomething('Arabic','Beginner')" id="rdo-3" name="radio-grp">
-			  <svg width="20px" height="20px" viewBox="0 0 20 20">
+			  <svg class="option-3-arabic" width="20px" height="20px" viewBox="0 0 20 20">
 				<circle cx="10" cy="10" r="9"></circle>
 				<path d="M10,7 C8.34314575,7 7,8.34314575 7,10 C7,11.6568542 8.34314575,13 10,13 C11.6568542,13 13,11.6568542 13,10 C13,8.34314575 11.6568542,7 10,7 Z" class="inner"></path>
 				<path d="M10,1 L10,1 L10,1 C14.9705627,1 19,5.02943725 19,10 L19,10 L19,10 C19,14.9705627 14.9705627,19 10,19 L10,19 L10,19 C5.02943725,19 1,14.9705627 1,10 L1,10 L1,10 C1,5.02943725 5.02943725,1 10,1 L10,1 Z" class="outer"></path>
