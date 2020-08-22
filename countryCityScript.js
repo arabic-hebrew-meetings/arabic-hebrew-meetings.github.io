0084e6adda21dae07e126ed702495bca26734e4c
arabic-hebrew-meetings <34382029+arabic-hebrew-meetings@users.noreply.github.com>
2020-08-23 01:12:39 +0300

    Add files via upload

--- countryCityScript.js
+++ countryCityScript.js
@@ -1,3 +1,7 @@
+var i;
+var x;
+var hasListener = false;
+
 var past = [];
 var cur = -1;
 function getNext(){
@@ -5,7 +9,7 @@ function getNext(){
 	saveAction("countryCity", "getNext", {cur: cur});
 	if (cur === past.length) {
 		while (true) {
-		var i = 0;	
+		i = 0;	
 		if 	((cur < (hebrewLetters.length*2)) && ((cur%2) === 0)) {
 			i = Math.floor(Math.random() * hebrewLetters.length);
 		} else {
@@ -54,35 +58,55 @@ function getPrevButton(cur) {
 		  <span class="glyphicon glyphicon glyphicon-chevron-right my-activity-button-single"></span>
         </a>`;
 	if (cur === 0) {
-		prevButton = `<a class="circle-button invisible" onclick="getPrev()" role="button">
+		if (x.matches) { // small screen
+			prevButton = ``;
+		} else {
+			prevButton = `<a class="circle-button invisible" onclick="getPrev()" role="button">
 		  <span class="glyphicon glyphicon glyphicon-chevron-right my-activity-button-single"></span>
         </a>`;
+		}
 	}	
 	return prevButton;
 }
 
 function displayContent(cur, i) {
+	x = window.matchMedia("(max-width: 600px)")
+	displayContentByWidth() // Call listener function at run time
+	if (!hasListener) {
+		x.addListener(displayContentByWidth) // Attach listener function on state changes
+		hasListener = true;
+	}
+}
+
+function displayContentByWidth() {
 	nextButton = getNextButton(cur);
 	prevButton = getPrevButton(cur);
 	content = getContentRectWithSpecificOrder();
-	console.log(content)
-	document.getElementById("button-and-text").innerHTML = nextButton + content + prevButton;
-	displayContentWithSpecificOrder(i);
+	displayContentAndButtons();
+	displayContentBySpecificOrder(i);
+}
+
+function displayContentAndButtons() {
+	if (x.matches) { // small screen
+		document.getElementById("button-and-text").innerHTML = `<div class="row">` + content + `</div><div class="row">` + prevButton + nextButton + `</div>`;
+	} else {
+		document.getElementById("button-and-text").innerHTML =  prevButton + content + nextButton;
+	}
 }
 
 function getContentRectWithSpecificOrder() {
 	content = `<div class="rectangle rtl">
-                <h2 class="rtl" id="language1"></h2>
-				<h2 class="rtl" id="language2"></h2>
+                <h2 class="rtl activityContent" id="language1"></h2>
+				<h2 class="rtl activityContent" id="language2"></h2>
 		<h1 class="rtl" id="letter"></h1>
-		<h2><br>הקטגוריות - الفئات:</h2>
-		<h2>ארץ - עיר - חי - צומח - דומם - שם - מקצוע - אישיות</h2>
-		<h2>بلاد - مدينة - حيوان - نبات - جماد - اسم - مهنة - شخصية مشهورة</h2>
+		<h2 class="rtl activityContent"><br>הקטגוריות - الفئات:</h2>
+		<h2 class="rtl activityContent">ארץ - עיר - חי - צומח - דומם - שם - מקצוע - אישיות</h2>
+		<h2 class="rtl activityContent">بلاد - مدينة - حيوان - نبات - جماد - اسم - مهنة - شخصية مشهورة</h2>
             </div>`;
 	return content;		
 }
 
-function displayContentWithSpecificOrder(i) {
+function displayContentBySpecificOrder(i) {
 	if (i < hebrewLetters.length) {
 		document.getElementById("language1").innerHTML = hebrewLetterText1;
 		document.getElementById("language2").innerHTML = hebrewLetterText2;
